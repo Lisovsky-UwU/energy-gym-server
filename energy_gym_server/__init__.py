5ca0d52e6d6d764bb33ef98f89dd6c1c756e2f3b
Lisovsky_UwU <nikish10012@gmail.com>
2022-09-26 16:58:29 +0700

    initialize commit

---
+++ energy_gym_server/__init__.py
@@ -0,0 +1,8 @@
+
+
+
+def start():
+    try:
+        ...
+    except KeyboardInterrupt:
+        pass
---
+++ run.py
@@ -0,0 +1,5 @@
+from energy_gym_server import start
+
+
+if __name__ == '__main__':
+    start()
