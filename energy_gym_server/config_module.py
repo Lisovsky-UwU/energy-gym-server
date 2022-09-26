577230d293ea53bccf4ae01081b51a80aafcc133
Lisovsky_UwU <nikish10012@gmail.com>
2022-09-26 18:01:09 +0700

    Обработка исключений на чтение конфига

--- energy_gym_server/config_module.py
+++ energy_gym_server/config_module.py
@@ -1,6 +1,8 @@
 import configparser
 from pydantic import BaseModel, validator, ValidationError
 
+from .exception import ConfigReadException
+
 
 class Config(BaseModel):
     use_dev: bool
@@ -12,11 +14,15 @@ class Config(BaseModel):
             raise ValidationError('Недопустимое значение порта')
         return value
 
+try:
+    config_pars = configparser.ConfigParser()
+    config_pars.read('config.ini')
 
-config_pars = configparser.ConfigParser()
-config_pars.read('config.ini')
-
-config = Config(
-    use_dev=config_pars['DEFAULT']['UseDev'],
-    server_port=config_pars['DEFAULT']['Port']
-)
+    config = Config(
+        use_dev=config_pars['DEFAULT']['UseDev'],
+        server_port=config_pars['DEFAULT']['Port']
+    )
+except ValidationError as e:
+    raise ConfigReadException(f'Ошибка валидирования значений: {e}')
+except KeyError as e:
+    raise ConfigReadException(f'Ошибка чтения значения из конфига: {e}')
