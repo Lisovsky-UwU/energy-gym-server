4aba03b48d4c50a31f75e8918a8e6c3686fc17f1
Lisovsky_UwU <nikish10012@gmail.com>
2022-09-29 14:08:52 +0700

    Добавил фабрику сессий

--- energy_gym_server/models/database/__init__.py
+++ energy_gym_server/models/database/__init__.py
@@ -1,11 +1,27 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from asyncio import current_task
+from sqlalchemy.ext.asyncio import create_async_engine, async_scoped_session, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 from ...config_module import config
 
 
 engine = create_async_engine(config.postgre.connect_str, future=True)
 Base = declarative_base(bind=engine)
+
+
+def session_factory(**kwargs):
+    return async_scoped_session(
+        sessionmaker(
+            bind=engine,
+            autocommit=False,
+            autoflush=False,
+            class_=AsyncSession,
+            future=True,
+            **kwargs
+        ),
+        scopefunc=current_task
+    )
 
 
 from .available_days import AvailableDays
