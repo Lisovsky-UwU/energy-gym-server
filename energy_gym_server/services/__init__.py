27e6855cc9486f560086a30b03315277525ed925
Lisovsky_UwU <nikish10012@gmail.com>
2022-10-07 16:45:43 +0700

    user -> authorization rename

--- energy_gym_server/services/__init__.py
+++ energy_gym_server/services/__init__.py
@@ -1,4 +1,4 @@
 from .available_days import AvailableDaysService
 from .students import StudentsService
 from .entries import EntriesService
-from .user import AuthorizationService
+from .authorization import AuthorizationService
